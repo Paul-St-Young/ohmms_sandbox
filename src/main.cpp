44f27705b040ce29b4ddcb9b77f94c93fda81809
Paul Young <yyang173@illinois.edu>
2016-10-25 10:46:53 -0500

    used OhmmsXPathObject

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
+#include <iostream>
+#include <assert.h>
 #include "OhmmsData/Libxml2Doc.h"
 //#include "OhmmsData/AttributeSet.h"
+
+using namespace std;
+
+/*
 #include <libxml/parser.h>
 #include <libxml/xpath.h>
 #include <string>
-#include <iostream>
 
-using namespace std;
 
 ostringstream& project_descriptions(ostringstream& os, string xmlfile, string xpath_exp="//project"){
 
@@ -28,19 +32,47 @@ ostringstream& project_descriptions(ostringstream& os, string xmlfile, string xp
 
   return os;
 
-  /* to use, make 3 lines in main()
-  string xmlfile="simple.xml";
-  ostringstream os;
-  cout << project_descriptions(os,xmlfile).str();
-  */
+  //// to use, make 3 lines in main()
+  //string xmlfile="simple.xml";
+  //ostringstream os;
+  //cout << project_descriptions(os,xmlfile).str();
+
+}
+*/
+
+ostringstream& list_nodeset(ostringstream& os, xmlDocPtr doc, xmlNodeSetPtr nodeset)
+{ // for each node in nodeset, print name with text 
+
+  // temperary variables
+  xmlNodePtr cur;
+  xmlChar* text;
+
+  for (int i=0;i<nodeset->nodeNr;i++){
+    cur  = nodeset->nodeTab[i];
+    text = xmlNodeListGetString(doc,cur->xmlChildrenNode,1);
+    os << cur->name << ": " << text << endl;
+    xmlFree(text);
+  }
+
+  return os;
 
 }
 
-int main(){
+int main()
+{
 
+  // parse xml document
   Libxml2Document doc;
-  bool okay = doc.parse("simple.xml");
-  cout << okay << endl;
+  bool okay = doc.parse("bcc_1.31.xml");
+  assert(okay==1);
+
+  // use xpath to search document
+  OhmmsXPathObject xpath_obj = OhmmsXPathObject("//parameter",doc.getRoot());
+  ostringstream os;
+  list_nodeset(os,doc.getDocument(),xpath_obj.result->nodesetval);
+
+  cout << os.str();
+
 
   return 0;
 }
